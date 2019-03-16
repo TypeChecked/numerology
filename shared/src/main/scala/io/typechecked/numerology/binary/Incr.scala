340946f6d8cd7fb94db1ad8f98124d75a0e0d013
jdrphillips <phillips101@gmail.com>
2019-03-16 09:39:14 +0000

    BNat Incr

---
+++ shared/src/main/scala/io/typechecked/numerology/binary/Incr.scala
@@ -0,0 +1,17 @@
+package io.typechecked
+package numerology
+package binary
+
+import BNat.b1
+
+trait Incr[A <: BNat] { type Out <: BNat }
+
+object Incr {
+
+  type Aux[A <: BNat, B <: BNat] = Incr[A] { type Out = B }
+
+  implicit def incrZero: Incr.Aux[b0, b1] = null
+  implicit def incrEven[A <: BNat]: Incr.Aux[Zero[A], One[A]] = null
+  implicit def incrOdd[A <: BNat, B <: BNat](implicit i: Incr.Aux[A, B]): Incr.Aux[One[A], Zero[B]] = null
+
+}
