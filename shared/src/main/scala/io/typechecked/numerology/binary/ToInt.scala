4de0d64e485fe6ac2c081e1353e45b98ed8b665e
jdrphillips <me@jdrphillips.io>
2019-03-23 09:02:10 +0000

    Remove unnecessary ToInt base cases

--- shared/src/main/scala/io/typechecked/numerology/binary/ToInt.scala
+++ shared/src/main/scala/io/typechecked/numerology/binary/ToInt.scala
@@ -6,9 +6,6 @@ trait ToInt[T <: BNat] { def value: Int }
 
 object ToInt {
 
-  implicit val baseCase: ToInt[b0] =
-    new ToInt[b0] { val value: Int = 0 }
-
   implicit def longCase[T <: BNat](
     implicit lte: T Lte BNat.MaxInt,
     toLong: ToLong[T]
--- shared/src/main/scala/io/typechecked/numerology/ternary/ToInt.scala
+++ shared/src/main/scala/io/typechecked/numerology/ternary/ToInt.scala
@@ -6,9 +6,6 @@ trait ToInt[T <: TNat] { def value: Int }
 
 object ToInt {
 
-  implicit val baseCase: ToInt[t0] =
-    new ToInt[t0] { val value: Int = 0 }
-
   implicit def longCase[T <: TNat](
     implicit lte: T Lte TNat.MaxInt,
     toLong: ToLong[T]
