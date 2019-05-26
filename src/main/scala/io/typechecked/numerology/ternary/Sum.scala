48dadfd4ce1dfe11eb0f2c02d3231b05df8aaa5c
jdrphillips <me@jdrphillips.io>
2019-05-26 19:55:15 +0100

    Tighten ternary Sum and add comments

--- src/main/scala/io/typechecked/numerology/ternary/Sum.scala
+++ src/main/scala/io/typechecked/numerology/ternary/Sum.scala
@@ -8,36 +8,46 @@ object Sum {
 
   type Aux[A <: TNat, B <: TNat, Out0 <: TNat] = Sum[A, B] { type Out = Out0 }
 
-  implicit def zeroSumA[A <: TNonZero]: Sum.Aux[A, t0, A] = null
-  implicit def zeroSumB[B <: TNonZero]: Sum.Aux[t0, B, B] = null
-  implicit def zeroSum0: Sum.Aux[t0, t0, t0] = null
+  // A + 0 = A for all A
+  implicit def caseA0[A <: TNat]: Sum.Aux[A, t0, A] = null
+  // 0 + B = B for all non-zero B
+  implicit def case0B[B <: TNonZero]: Sum.Aux[t0, B, B] = null
 
-  implicit def sumZO1[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[Zero[A], One[B], One[s.Out]] = null
-  implicit def sumZO2[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[One[A], Zero[B], One[s.Out]] = null
+  // 3A + (3B + 1) = 3(A + B) + 1
+  implicit def caseZO[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[Zero[A], One[B], One[s.Out]] = null
+  // (3A + 1) + 3B = 3(A + B) + 1
+  implicit def caseOZ[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[One[A], Zero[B], One[s.Out]] = null
 
-  implicit def sumZT1[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[Zero[A], Two[B], Two[s.Out]] = null
-  implicit def sumZT2[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[Two[A], Zero[B], Two[s.Out]] = null
+  // 3A + (3B + 2) = 3(A + B) + 2
+  implicit def caseZT1[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[Zero[A], Two[B], Two[s.Out]] = null
+  // (3A + 2) + 3B = 3(A + B) + 2
+  implicit def caseZT2[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[Two[A], Zero[B], Two[s.Out]] = null
 
-  implicit def sumTO1[A <: TNat, B <: TNat, SOut <: TNat, SIncr <: TNat](
+  // (3A + 1) + (3B + 2) = 3(A + B + 1)
+  implicit def caseOT[A <: TNat, B <: TNat, SOut <: TNat, SIncr <: TNat](
     implicit s: Sum.Aux[A, B, SOut],
     incr: Incr.Aux[SOut, SIncr],
     treble: Treble[SIncr]
   ): Sum.Aux[One[A], Two[B], treble.Out] = null
 
-  implicit def sumTO2[A <: TNat, B <: TNat, SOut <: TNat, SIncr <: TNat](
+  // (3A + 2) + (3B + 1) = 3(A + B + 1)
+  implicit def caseTO[A <: TNat, B <: TNat, SOut <: TNat, SIncr <: TNat](
     implicit s: Sum.Aux[A, B, SOut],
     incr: Incr.Aux[SOut, SIncr],
     treble: Treble[SIncr]
   ): Sum.Aux[Two[A], One[B], treble.Out] = null
 
-  implicit def sumZZ[A <: TNat, B <: TNat, AB <: TNat](
+  // 3A + 3B = 3(A + B)
+  implicit def caseZZ[A <: TNat, B <: TNat, AB <: TNat](
     implicit s: Sum.Aux[A, B, AB],
     treble: Treble[AB]
   ): Sum.Aux[Zero[A], Zero[B], treble.Out] = null
 
-  implicit def sumOO[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[One[A], One[B], Two[s.Out]] = null
+  // (3A + 1) + (3B + 1) = 3(A + B) + 2
+  implicit def caseOO[A <: TNat, B <: TNat](implicit s: Sum[A, B]): Sum.Aux[One[A], One[B], Two[s.Out]] = null
 
-  implicit def sumTT[A <: TNat, B <: TNat, SOut <: TNat](
+  // (3A + 2) + (3B + 2) = 3(A + B + 1) + 1
+  implicit def caseTT[A <: TNat, B <: TNat, SOut <: TNat](
     implicit s: Sum.Aux[A, B, SOut],
     incr: Incr[SOut]
   ): Sum.Aux[Two[A], Two[B], One[incr.Out]] = null
